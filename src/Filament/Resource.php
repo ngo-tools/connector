e3938e3942418dde119ca7ec040586042231b4f4
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2025-01-02 17:30:54 +0100

    Update Resource.php

--- src/Filament/Resource.php
+++ src/Filament/Resource.php
@@ -2,15 +2,29 @@
 
 namespace NgoTools\Connector\Filament;
 
+use Illuminate\Contracts\Support\Htmlable;
+use ReflectionClass;
+
 abstract class Resource extends \Filament\Resources\Resource
 {
     public final static function canAccess(): bool
     {
+        $namespace = implode('\\', array_slice(explode('\\', (new ReflectionClass(static::class))->getNamespaceName()), 0, 2));
+
+        // check if app is active
+        
+        dd($namespace);
+
         return static::hasAccess();
     }
 
     public static function hasAccess(): bool
     {
         return parent::canAccess();
+    }
+
+    public final static function getNavigationIcon(): string|Htmlable|null
+    {
+        return '';
     }
 }
