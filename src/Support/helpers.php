824eaf117f1f5f39fce7cfa3d49dc9dbd06dd9e1
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2025-04-10 02:03:49 +0200

    Implement auth check in tenantRoute()

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -12,8 +12,20 @@ if (! function_exists('ngo')) {
 }
 
 if (! function_exists('tenantRoute')) {
-    function tenantRoute($name, $parameters = [], $absolute = true)
+    function tenantRoute($name, $parameters = [], $absolute = true, $skipAuthenticationCheck = false)
     {
+        if(!$skipAuthenticationCheck) {
+            $controller = Route::getRoutes()->getByName($name)->getController();
+
+            if(!$controller::canAccess()) {
+                return null;
+            }
+
+            if(filled($controller::getResource()) && !$controller::getResource()::canAccess()) {
+                return null;
+            }
+        }
+
         // Helper for the case that we only have one parameter - we assume "record"
         if(!is_array($parameters)) {
             $parameters = ['record' => $parameters];
