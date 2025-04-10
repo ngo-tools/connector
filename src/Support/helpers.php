562926a9a8908c15995cd79fbe233dcd45763efc
holybiber <samuel@holydevelopers.net>
2025-04-10 14:47:23 +0200

    Make tenantRoute even more convenient

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -12,8 +12,20 @@ if (! function_exists('ngo')) {
 }
 
 if (! function_exists('tenantRoute')) {
-    function tenantRoute($name, $parameters = [], $absolute = true, $skipAuthenticationCheck = false): ?string
+    /**
+     * Return the route if it is accessible for the user, otherwise return null.
+     * @param mixed $parameters Can be an array, an object or null.
+     * The null value is to simplify this common pattern:
+     *     ->url(fn($record) => $record->contact? tenantRoute('...', $record->contact) : null),
+     * into
+     *     ->url(fn($record) => tenantRoute('...', $record->contact))
+     */
+    function tenantRoute($name, $parameters = null, $absolute = true, $skipAuthenticationCheck = false): ?string
     {
+        if ($parameters === null) {
+            return null;
+        }
+
         if(!$skipAuthenticationCheck && !canAccessRoute($name)) {
             return null;
         }
