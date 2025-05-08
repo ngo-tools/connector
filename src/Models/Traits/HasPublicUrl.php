c08b93ee7edc6317d797267b227f85719f88a3cd
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2025-05-08 18:55:18 +0200

    Update HasPublicUrl.php

--- src/Models/Traits/HasPublicUrl.php
+++ src/Models/Traits/HasPublicUrl.php
@@ -2,7 +2,39 @@
 
 namespace NgoTools\Connector\Models\Traits;
 
+use App\Enums\PublicUrlType;
+use App\Models\PublicUrl;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
 trait HasPublicUrl
 {
-    use App\Models\Traits\HasPublicUrl;
+    public function publicUrls($includeInactive = false) : MorphMany
+    {
+        return $this->morphMany(PublicUrl::class, 'model')
+            ->where('type', PublicUrlType::Package)
+            ->when($includeInactive, fn(Builder $query) => $query->withoutGlobalScope('active'));
+    }
+
+    public function getPublicUrl(Carbon $expiresAt = null, $data = [], $version = null) : PublicUrl
+    {
+        return $this->publicUrls()->firstOrCreate([
+            'type' => PublicUrlType::Package,
+            'version' => $version,
+        ], [
+            'expires_at' => $expiresAt,
+            'data' => $data,
+        ]);
+    }
+
+    public function publicUrlIsActive() : bool
+    {
+        return $this->publicUrls()->exists();
+    }
+
+    public function refreshPublicUrl($version = null)
+    {
+        return $this->getPublicUrl(version: $version)->refreshToken();
+    }
 }
